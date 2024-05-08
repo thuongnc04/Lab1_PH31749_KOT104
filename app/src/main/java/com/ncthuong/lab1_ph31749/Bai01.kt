82f1fa6ae55626fe3eec68e5b3a2f5b3244c8412
thuongnc04 <nct201204@gmail.com>
2024-05-08 22:03:49 +0700

    ph31749

---
+++ app/src/main/java/com/ncthuong/lab1_ph31749/Bai01.kt
@@ -0,0 +1,19 @@
+package com.ncthuong.lab1_ph31749
+
+
+
+fun main() {
+    print("Nguyễn Công Thưởng - PH31749\n")
+    print("=========================\n")
+    println("Quanh năm buôn bán ở mom sông")
+    println("Nuôi đủ năm con với một chồng")
+    println("\tlặn lội thân cờ khi quãng vắng")
+    println("\teo sèo mặt nước buổi đò đồng")
+    println("Một duyên hai nợ âu đành phận")
+    println("Năm nắng mười mưa há chẳng công")
+    println("\tCha mẹ thói đời \"ăn ở bạc\"")
+    println("\tCó chồng hờ hững cũng như không")
+}
+
+class Bai01 {
+}
---
+++ app/src/main/java/com/ncthuong/lab1_ph31749/Bai02.kt
@@ -0,0 +1,19 @@
+package com.ncthuong.lab1_ph31749
+
+fun main() {
+    var a = 0.0
+    var b = 0.0
+    println("Nhập a:")
+    var s = readLine()
+    if (s != null) a = s.toDouble()
+    println("Nhập b:")
+    s = readLine()
+    if (s != null) b = s.toDouble()
+    println("Tổng: " + (a + b))
+    println("Hiệu: " + (a - b))
+    println("Tích: " + (a * b))
+    println("Thương: " + (a / b))
+}
+
+class Bai02 {
+}
